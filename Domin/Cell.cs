edab0b1c1fe6346eff0f9c676745d89192e89a74
lgvuanh4107-wq <lgvuanh4107@gmail.com>
2026-09-06 09:28:57 +0700

    Add Cell class to store cell state

---
+++ Domin/Cell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domin
+{
+    namespace Domin
+    {
+        public class Cell
+        {
+            public bool IsMine { get; set; }
+            public bool IsRevealed { get; set; }
+            public bool IsFlagged { get; set; }
+            public int AdjacentMines { get; set; }
+        }
+    }
+}
